ac01fce89f33c2e1f8b1d55de81d9da771366b9e
Vladyslav <46854131+Vladkarok@users.noreply.github.com>
2022-04-29 16:51:26 +0300

    Update data.tf

--- data.tf
+++ data.tf
@@ -20,23 +20,6 @@ data "aws_route53_zone" "primary" {
   name = var.domain
 }
 
-#############
-## Outputs ##
-#############
-output "latest_ubuntu_ami_id" {
-  description = "Ubuntu 20.04 LTST AMI ID"
-  value       = data.aws_ami.ubuntu_latest.id
-}
-
-output "latest_amazon_linux_ami_id" {
-  description = "Amazon Linux 2 AMI ID"
-  value       = data.aws_ami.amazon_linux_latest.id
-}
-
-output "domain" {
-  description = "Route53 ZONE ID"
-  value       = data.aws_route53_zone.primary.zone_id
-}
 
 ###############
 ## Variables ##
